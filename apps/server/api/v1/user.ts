3a007187519f6792341b224baf644cfe8cc82dae
Anshul Bharat <anshul.ab17x@gmail.com>
2026-02-01 12:54:48 +0530

    p-2

--- apps/server/api/v1/user.ts
+++ apps/server/api/v1/user.ts
@@ -3,6 +3,7 @@ import { Hono } from "hono";
 const user = new Hono();
 
 user.get("/", (c) => c.text("user root"));
+
 user.get("/:id", (c) => c.text(`user ${c.req.param("id")}`));
 
 export default user;
